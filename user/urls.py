d5c44ddfa9cbf76a4f80375753e674fa64742858
tandukar <84461389+tandukar@users.noreply.github.com>
2022-04-29 19:55:22 +0545

    Edit user profile
    Adding edit user profile

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView,UserEditView
 
 urlpatterns = [
      path('register/', UserRegisterView.as_view(), name="register"),
+     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -9,4 +9,15 @@ class UserRegisterView(generic.CreateView):
     form_class=UserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+
+class UserEditView(generic.UpdateView):
+    form_class=UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('homepage')
+
+
+    def get_object(self):
+        return self.request.user
     
