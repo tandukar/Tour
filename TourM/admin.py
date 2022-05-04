b0c8edbd4eab72843ee1741e621d218b77363075
Sameer <np03cs4s210060@heraldcollege.edu.np>
2022-05-04 17:18:57 +0545

    update
    models creation
    made migration
    register contact(from model) in admin.py

--- TourM/admin.py
+++ TourM/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post, Provinces, Comment
+from TourM.models import Contact
 # Register your models here.
 
 admin.site.register(Post) 
 admin.site.register(Provinces) 
 admin.site.register(Comment) 
+
+#registering the model in admin
+admin.site.register(Contact)
