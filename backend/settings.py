59552e16f1d5cd741b8ae00731bbc047491b5ef2
Sameer <np03cs4s210060@heraldcollege.edu.np>
2022-05-04 17:23:54 +0545

    update
    installing the app from the tour>app.py to the backend>setting.py

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'TOURM.apps.TourmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
