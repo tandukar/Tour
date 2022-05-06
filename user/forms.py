777fbc83b99d342e33dd21d76b84d7de449cd1a3
tandukar <84461389+tandukar@users.noreply.github.com>
2022-05-06 23:27:25 +0545

    Final touches to register

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
+from logging import PlaceHolder
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm,PasswordChangeForm
+from django.contrib.auth.forms import UserChangeForm,PasswordChangeForm, UserCreationForm
 
 
 
@@ -33,11 +34,23 @@ class PasswordChangedForm(PasswordChangeForm):
      old_password=forms.CharField(max_length=30 ,widget=forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}))
      new_password1=forms.CharField(max_length=30 ,widget=forms.PasswordInput(attrs={'class': 'form-control' , 'type': 'password'}))
      new_password2=forms.CharField(max_length=30 ,widget=forms.PasswordInput(attrs={'class': 'form-control' , 'type': 'password'}))
-   
-
-
-
 
      class Meta:
          model = User 
          fields = ('old_password', 'new_password1', 'new_password2' )
+
+
+
+class registerform(UserCreationForm):
+    
+     username=forms.CharField(label="",max_length=30 ,widget=forms.TextInput(attrs={'class': 'form-control', 'PlaceHolder':'Username'  }))
+     email=forms.EmailField(label="",widget=forms.EmailInput(attrs={'class': 'form-control', 'PlaceHolder':'Email' }))
+     password1=forms.CharField(label="",max_length=30 ,widget=forms.PasswordInput(attrs={'class': 'form-control' , 'type': 'password', 'PlaceHolder':'Password' }))
+     password2=forms.CharField(label="",max_length=30 ,widget=forms.PasswordInput(attrs={'class': 'form-control' , 'type': 'password', 'PlaceHolder':'Confirm password' }))
+   
+
+     class Meta:
+         model = User 
+         fields = ('username', 'email', 'password1','password2' )
+
+
--- user/views.py
+++ user/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm 
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 
-from .forms import EditProfileForm,PasswordChangedForm
+
+from .forms import EditProfileForm,PasswordChangedForm,registerform
 from django.contrib import messages
 # Create your views here.
 
 
 class UserRegisterView(generic.CreateView):
-    form_class=UserCreationForm
+    
+
+    form_class=registerform
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
