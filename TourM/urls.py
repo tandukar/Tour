a1f7aa513111b571b1eeb3b2684a76567ef50f9f
tandukar <84461389+tandukar@users.noreply.github.com>
2022-05-09 18:28:17 +0545

    configure seasons

--- TourM/urls.py
+++ TourM/urls.py
@@ -5,21 +5,17 @@ from .  views import ad_homeView, enroll, pack_detailView, homeView,UpdatePostVi
 urlpatterns = [
     path('ad-home/', ad_homeView.as_view(), name="ad_homeView"),
     path('details/edit/<int:pk>/', UpdatePostView.as_view(), name="UpdatePost"),
-    path('details/<int:pk>/delete', DeletePostView.as_view(), name="DeletePost"),
-   
+    path('details/<int:pk>/delete', DeletePostView.as_view(), name="DeletePost"),  
     path('info/', views.infoView, name="infoView"),
     path('packView/', views.PackView, name="PackView"),
     path('package/', homeView.as_view(), name="homeView"),
     path('details/<int:pk>', pack_detailView.as_view(), name="pack_detailView"),
-
     path('enroll/', enroll.as_view(), name="enrollView"),
-
     path('details/<int:pk>/checkout', EnrollView.as_view(), name="checkout"),
-
     path('user_csv', views.user_csv, name="user_csv"),
     path('user_pdf', views.user_pdf, name="user_pdf"),
-
     path('enrolled/', views.enrolled, name="enrolled"),
+    path('seasons/', views.seasons, name="seasons"),
     
     path('',views.homepage, name="homepage"),
     
